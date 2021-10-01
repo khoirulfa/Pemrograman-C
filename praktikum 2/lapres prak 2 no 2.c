adbde56327a1824409365dec135701a798bb9711
argarafiar <89307621+argarafiar@users.noreply.github.com>
2021-10-01 15:20:38 +0700

    menambah program tiket di no 2 (hacktoberfest)

--- praktikum 2/lapres prak 2 no 2.c
+++ praktikum 2/lapres prak 2 no 2.c
@@ -2,8 +2,21 @@
 #include <stdlib.h>
 void main()
 {
-    int value1, value2, sum;
-    value1 = 35;
-    value2 = 18;
-    printf("The sum of %d and %d is %d\n", value1, value2, sum);
+//    int value1, value2, sum;
+//    value1 = 35;
+//    value2 = 18;
+//    printf("The sum of %d and %d is %d\n", value1, value2, sum);
+    
+    printf("======Tugas pendahuluan 3======\n");
+    int jumlah3, sisa3, jumtiket, total;
+
+    printf("\nberapa banyak tiket yang mau di pesan? ");
+    scanf("%d", &jumtiket);
+
+    jumlah3 = jumtiket/3;
+    sisa3 = jumtiket%3;
+    total = jumlah3 * (2*50000) + sisa3 * 50000;
+
+    printf("total harga : Rp.%d\n", total);
+
 }
