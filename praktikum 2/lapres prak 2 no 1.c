0fe9187c981799c94dbdf643d2ada02e7e31396d
argarafiar <89307621+argarafiar@users.noreply.github.com>
2021-10-01 18:26:00 +0700

    membuat program angka segitiga (hacktoberfest)

--- praktikum 2/lapres prak 2 no 1.c
+++ praktikum 2/lapres prak 2 no 1.c
@@ -2,10 +2,22 @@
 #include <stdlib.h>
 void main()
 {
-    int jawab, hasil;
-    jawab = 100;
-    hasil = jawab - 10;
-    printf("Jawabannya adalah %d\n", hasil + 6);
+//    int jawab, hasil;
+//    jawab = 100;
+//    hasil = jawab - 10;
+//    printf("Jawabannya adalah %d\n", hasil + 6);
     
-    printf("hello world!);
+//    printf("hello world!);
+    printf("=====soal pendahuluan no-6=====\n");
+    int max, i, b;
+
+    printf("Masukkan angka maksimal : ");
+    scanf("%d", &max);
+    for(i=1; i<=max; i++)
+    {
+        for(b=1; b<=i; b++)
+            printf("%d ", b);
+        printf("\n");
+    }
+
 }
